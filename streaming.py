8cbaf4fb09f4be90f86da68ecc627b9e6a3ba07d
jfrshd <jfrshd94@gmail.com>
2020-01-20 22:52:23 +0200

    Tweek

--- streaming.py
+++ streaming.py
@@ -22,3 +22,21 @@ class MyListener(StreamListener):
 
 twitter_stream = Stream(auth, MyListener())
 twitter_stream.filter(track=['#python'])
+
+
+"""
+We have introduced tweepy as a tool to access Twitter data in a fairly easy way with Python.
+There are different types of data we can collect, with the obvious focus on the “tweet” object.
+Once we have collected some data, the possibilities in terms of analytics applications are endless.
+In the next episodes, we’ll discuss some options.
+"""
+
+"""
+A project that collects twitter data using twitter api. Then data is preprocessed, analysed and sentimentally analysed; results are then visualized in charts and interactive maps.
+Also sentiment analysis is done
+Part 3: Term Frequencies
+Part 4: Rugby and Term Co-Occurrences
+Part 5: Data Visualisation Basics
+Part 6: Sentiment Analysis Basics
+Part 7: Geolocation and Interactive Maps
+"""
